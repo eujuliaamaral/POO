a2f4ea8ab5d80a508cbd6ec0bdb562494558ae23
MikaelAbdias <mikaelabdias28@gmail.com>
2026-06-27 01:26:30 -0300

    Herança de Profissonal e encapsulameto Pessoa/Paciente

---
+++ Pessoa.java
@@ -0,0 +1,55 @@
+public abstract class Pessoa {
+
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String dataNascimento;
+
+    public Pessoa(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public Pessoa(String nome, String cpf, String telefone, String dataNascimento) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        if(cpf = cpf.trim();)
+            this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public abstract void exibirResumo();
+
+}
